a3a952bee347507621b882db6765c523b3481039
Yasmin Braz A Silva <141586083+YasminBrazASilva@users.noreply.github.com>
2023-11-02 18:41:24 -0300

    Add files via upload

--- Exercicio4_3_5.sql
+++ Exercicio4_3_5.sql
@@ -1,9 +1,9 @@
 -- Insere dados na tabela veiculos
-CALL novoVeiculo(301, 'Fusca', 'Preto', 'WER-3456', 30);
-CALL novoVeiculo(302, 'Variante', 'Rosa', 'FDS-8384', 60);
-CALL novoVeiculo(303, 'Comodoro', 'Preto', 'CVB-9933', 20);
-CALL novoVeiculo(304, 'Deloriam', 'Azul', 'FGH-2256', 80);
-CALL novoVeiculo(305, 'Brasília', 'Amarela', 'DDI-3948', 45);
+CALL novoVeiculo(301, 'WER-3456', 'Fusca', 'Preto', 30);
+CALL novoVeiculo(302, 'FDS-8384', 'Variante', 'Rosa', 60);
+CALL novoVeiculo(303, 'CVB-9933', 'Comodoro', 'Preto', 20);
+CALL novoVeiculo(304, 'FGH-2256', 'Deloriam', 'Azul', 80);
+CALL novoVeiculo(305, 'DDI-3948', 'Brasília', 'Amarela', 45);
 
 -- Exibe todas as linhas e colunas da tabela veiculos
 CALL selecionarTudo("veiculos");
